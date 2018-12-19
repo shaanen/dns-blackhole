237e6465f1b952e4e2e7297c0297f2cfcfd8c715
Cloud-init-user <zjors_93@hotmail.com>
2018-12-19 14:32:48 +0100

    PEP8

--- dns-blackhole.py
+++ dns-blackhole.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3.6
 # encoding: utf-8
-'''
-'''
+
+
 import yaml
 import requests
 import os
@@ -47,12 +47,14 @@ def load_config():
 def get_general(config):
     if 'dns-blackhole' in config:
         if 'general' in config['dns-blackhole']:
-            if 'whitelist' in config['dns-blackhole']['general'] and config['dns-blackhole']['general']['whitelist'] is not None:
+            if 'whitelist' in config['dns-blackhole']['general'] and config[
+                    'dns-blackhole']['general']['whitelist'] is not None:
                 whitelist = config['dns-blackhole']['general']['whitelist']
             else:
                 whitelist = os.path.dirname(config_file) + '/whitelist'
 
-            if 'blacklist' in config['dns-blackhole']['general'] and config['dns-blackhole']['general']['blacklist'] is not None:
+            if 'blacklist' in config['dns-blackhole']['general'] and config[
+                    'dns-blackhole']['general']['blacklist'] is not None:
                 blacklist = config['dns-blackhole']['general']['blacklist']
             else:
                 blacklist = os.path.dirname(config_file) + '/blacklist'
@@ -97,11 +99,13 @@ def process_host_file_url(bh_list, white_list, host_file_urls):
             print('[!] Fetch and parse URL: {0}'.format(url))
             r = requests.get(url)
         except:
-            print('Request to {0} failed: {1}. Skipping source.'.format(url, sys.exc_info()[0]))
+            print('Request to {0} failed: {1}. Skipping source.'.format(url,
+                  sys.exc_info()[0]))
             continue
 
         if r.status_code != 200:
-            print('Incorrect return code from {0}: {1}. Skipping source.'.format(url, r.status_code))
+            print('Incorrect return code from {0}: {1}. Skipping source.'
+                  .format(url, r.status_code))
             continue
 
         used_sources.append(r.url)
@@ -155,13 +159,14 @@ def process_easylist_url(bh_list, white_list, easy_list_url):
             print('[!] Fetch and parse URL: {0}'.format(url))
             r = requests.get(url)
         except:
-            print('Request to {0} failed: {1}. Skipping source.'.format(url, sys.exc_info()[0]))
+            print('Request to {0} failed: {1}. Skipping source.'.format(url,
+                  sys.exc_info()[0]))
             continue
 
         if r.status_code != 200:
-            print('Incorrect return code from {0}: {1}. Skipping source.'.format(url, r.status_code))
+            print('Incorrect return code from {0}: {1}. Skipping source.'
+                  .format(url, r.status_code))
             continue
-            
 
         used_sources.append(r.url)
         for line in r.iter_lines():
@@ -220,17 +225,20 @@ def process_disconnect_url(bh_list, white_list, d_url, d_cat):
         print('[!] Fetch and parse URL: {0}'.format(d_url))
         r = requests.get(d_url)
     except:
-        print('Request to {0} failed: {1}. Skipping source.'.format(d_url, sys.exc_info()[0]))
+        print('Request to {0} failed: {1}. Skipping source.'.format(d_url,
+              sys.exc_info()[0]))
         return bh_list
 
     if r.status_code == 200:
         try:
             j = r.json()
         except:
-            print('{0} did not return a json dict. Skipping source.'.format(d_url))
+            print('{0} did not return a json dict. Skipping source.'.format(
+                  d_url))
             return bh_list
     else:
-        print('Incorrect return code from {0}: {1}. Skipping source.'.format(d_url, r.status_code))
+        print('Incorrect return code from {0}: {1}. Skipping source.'.format(
+              d_url, r.status_code))
         return bh_list
 
     if 'categories' in j:
@@ -248,7 +256,7 @@ def process_disconnect_url(bh_list, white_list, d_url, d_cat):
                                     if host not in white_list:
                                         bh_list.append(host[::-1])
     else:
-        print('"categories" key not found in dict returned from {0}. Skipping source.'.format(d_url))
+        print('No "categories" key from {0}. Skipping source.'.format(d_url))
     return bh_list
 
 
@@ -301,21 +309,24 @@ def build_bw_lists(bh_whitelist, bh_blacklist):
                     if not line.strip() == '':
                         black_list.append(line.strip())
     if white_list:
-        print('Using {0} domains from whitelist "{1}"'.format(len(white_list), bh_whitelist))
+        print('Using {0} domains from whitelist "{1}"'.format(len(white_list),
+              bh_whitelist))
         used_sources.append("Custom local whitelist")
     if black_list:
-        print('Using {0} domains from blacklist "{1}"'.format(len(black_list), bh_blacklist))
+        print('Using {0} domains from blacklist "{1}"'.format(len(black_list),
+              bh_blacklist))
         used_sources.append("Custom local blacklist")
     return white_list, black_list
 
 
-def make_zone_file(bh_list, zone_file, zone_file_dir, zone_data, prefix, suffix):
+def make_zone_file(bh_list, zone_file, zone_file_dir, zone_data, prefix,
+                   suffix):
     f = open(zone_file, 'w')
 
     # First print all sources as comments
     f.write("# Sources: \n")
     used_sources_commented = ["# " + u for u in used_sources]
-    f.write(("\n").join(used_sources_commented))
+    f.write("\n".join(used_sources_commented))
     f.write("\n")
 
     # Define Unbound specific view:
@@ -336,7 +347,7 @@ def make_zone_file(bh_list, zone_file, zone_file_dir, zone_data, prefix, suffix)
         f.write(suffix)
     f.close()
 
-    # Create checksum file and move blacklistview and checksum file to specified dir
+    # Create checksum file and move along with blacklistview to specified dir
     open(zone_file + ".checksum", "w").write(sha256sum(zone_file))
     shutil.move(os.path.abspath(zone_file), zone_file_dir + zone_file)
     shutil.move(os.path.abspath(zone_file + ".checksum"), zone_file_dir +
@@ -348,11 +359,9 @@ def remove_subdomains(bh_list):
     bh_list.sort()
     bh_list_filtered = ["dummy_element"]
     for d in bh_list:
-        # Only add d to new list if d does not start with last element in new list
+        # Only add d to new list if d does not start with last item in new list
         if not d.find(bh_list_filtered[-1]) == 0:
             bh_list_filtered.append(d)
-        # else:
-        #     print("{0} starts with last element {1}".format(d, bh_list_filtered[-1]))
 
     # Remove dummy_element
     del bh_list_filtered[0]
@@ -375,7 +384,8 @@ def main():
     bh_white, bh_black = get_general(config)
 
     # Get service config
-    zone_file, zone_file_dir, zone_data, lists, prefix, suffix = get_service(config)
+    zone_file, zone_file_dir, zone_data, lists, prefix, suffix = \
+    get_service(config)
 
     # Build whitelist/blacklist
     white_list, black_list = build_bw_lists(bh_white, bh_black)
@@ -400,8 +410,6 @@ def main():
                                          d_url,
                                          d_cat)
 
-    # bh_list = [line.rstrip()[::-1] for line in open("10-domains.blacklist")]
-
     # Add hosts from blacklist
     bh_list = process_black_list(bh_list, black_list)
 
@@ -409,7 +417,8 @@ def main():
     bh_list = remove_subdomains(bh_list)
 
     # Create pdns file
-    make_zone_file(bh_list, zone_file, zone_file_dir, zone_data, prefix, suffix)
+    make_zone_file(bh_list, zone_file, zone_file_dir, zone_data, prefix,
+                   suffix)
 
 
 if __name__ == "__main__":
